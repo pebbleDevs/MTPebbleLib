d943e92284f16396537ec6197aafb985ef2e4749
Alok Choudhary <alok.c83@gmail.com>
2015-04-19 22:56:20 -0400

    First Function file
    text layer function add

---
+++ MTPebbleFunctions.h
@@ -0,0 +1,9 @@
+#pragma once
+	
+//func declaration	
+int libFunc(int justInt);
+TextLayer* textLayerWithAttributes(TextLayer *textLayer, 
+								   GRect layerRect, 
+								   GColor bgColor, 
+								   GColor textColor, 
+								   char *initialText);
